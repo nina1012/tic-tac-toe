78221c3fce4aa66de506550d0922c471c9291748
nina1012 <labferment10@gmail.com>
2021-05-13 12:05:58 +0200

    refactor iifes to classes

--- app.js
+++ app.js
@@ -1,75 +1,89 @@
-const Player = mark => {
-  let _mark = mark;
+const messageBox = document.querySelector('.message-box');
+const gameDom = document.querySelector('.game');
+const playAgainBtn = document.querySelector('.play-again');
+const fields = [...document.querySelectorAll('.field')];
 
-  getMark = () => _mark;
-  setMark = mark => (_mark = mark);
-
-  return {
-    getMark,
-    setMark
-  };
+// loads as soon as dom elements load
+const initialLoad = () => {
+  fields.forEach((field, i) =>
+    field.addEventListener('click', game.playerMove.bind(this, i))
+  );
+  game.board.resetBoard();
+  // set the currentPlayer to always start first to play
+  game.currentPlayer = undefined;
+  messageBox.classList.remove('show');
+  messageBox.classList.add('hide');
+  gameDom.style.filter = 'blur(0)';
+  document.querySelector('.options').style.display = 'flex';
 };
 
-// takes control over the board(fields)
-const GameBoard = (() => {
-  const _board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+playAgainBtn.addEventListener('click', e => {
+  e.target.parentNode.classList.remove('show');
+  setTimeout(() => {
+    fields.forEach(field => (field.textContent = ''));
+    initialLoad();
 
-  const getField = num => _board[num];
-  const setField = (player, num) => {
+    // show settings and hide board and players option
+    document.querySelector('.game').style.display = 'none';
+    document.querySelector('.settings').style.display = 'flex';
+    document.querySelector('.players').style.display = 'none';
+  }, 500);
+});
+
+// Player class //////
+class Player {
+  constructor(mark) {
+    this.mark = mark;
+  }
+
+  getMark() {
+    return this.mark;
+  }
+
+  setMark(mark) {
+    this.mark = mark;
+  }
+}
+
+// Board class ///////
+// takes control over the board(fields)
+class Board {
+  constructor() {
+    this.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  }
+
+  getField(num) {
+    return this.board[num];
+  }
+
+  setField(player, num) {
     const field = document.querySelector(`[data-number="${num}"]`);
     field.textContent = player.getMark();
-    _board[num] = player.getMark();
-  };
+    this.board[num] = player.getMark();
+  }
 
-  const availableFields = () => {
-    const available = [];
-    for (let i = 0; i < _board.length; i++) {
-      if (typeof _board[i] === 'number') {
-        available.push(i);
-      }
+  getAvailableFields() {
+    // filter only fields that are empty
+    return this.board.filter(field => typeof field === 'number');
+  }
+
+  resetBoard() {
+    for (let i = 0; i < this.board.length; i++) {
+      this.board[i] = i;
     }
-    return available;
-  };
-
-  // temp field for minimax function
-  const setTempField = (num, player) => {
-    if (player == undefined) {
-      _board[num] = num;
+  }
+  setTempField(num, player) {
+    if (player === undefined) {
+      this.board[num] = num;
       return;
     }
-    _board[num] = player.getMark();
-  };
-
-  // set board to initial state
-  const resetBoard = () => {
-    for (let i = 0; i < _board.length; i++) {
-      _board[i] = i;
-    }
-  };
-
-  return {
-    getField,
-    setField,
-    availableFields,
-    setTempField,
-    resetBoard
-  };
-})();
+    this.board[num] = player.getMark();
+  }
+}
 
 // controls the state of the game,players
-const GameController = (() => {
-  let _player1;
-  let _player2;
-  let _computer;
-  let _mode;
-
-  const getPlayer1 = () => _player1;
-  const getPlayer2 = () => _player2;
-  const getComputer = () => _computer;
-  const getMode = () => _mode;
-  const setMode = mode => (_mode = mode);
-
-  const _winningCombinations = [
+class Game {
+  static WINNING_COMBINATION = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -80,107 +94,134 @@ const GameController = (() => {
     [2, 4, 6]
   ];
 
-  const changeMark = (mark, mode) => {
+  board = new Board();
+  currentPlayer;
+
+  constructor(player1, player2, computer, mode) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.computer = computer;
+    this.mode = mode;
+  }
+
+  getMode() {
+    return this.mode;
+  }
+  setMode(mode) {
+    this.mode = mode;
+  }
+
+  startGame() {
+    // before game, clear the board and UI
+    if (this.mode === 'computer') {
+      if (this.player1.getMark() === 'o') {
+        // computer's turn
+        this.computerMove();
+      }
+    } else if (this.mode === 'multiple') {
+      // setting the currentPlayer
+      console.log(this.currentPlayer);
+      this.player1.getMark() === 'o'
+        ? (this.currentPlayer = this.player2)
+        : this.player1;
+    }
+  }
+
+  playerMove = num => {
+    const clickedField = this.board.getField(num);
+    // only fill the field if empty
+    if (this.mode === 'computer') {
+      if (
+        typeof clickedField === 'number' &&
+        !this.checkWin(this.board, this.computer)
+      ) {
+        this.board.setField(this.player1, num);
+        // check if player is already won or it's draw, otherwise, it's computer's turn
+        if (this.checkWin(this.board, this.player1)) {
+          this.endGame(this.player1);
+        } else if (this.checkDraw(this.board)) {
+          this.endGame('draw');
+        } else {
+          setTimeout(() => this.computerMove.call(this), 500);
+        }
+      }
+    } else if (this.mode === 'multiple') {
+      // if player1 chooses x, make it play first and x
+      if (this.currentPlayer === undefined) {
+        this.currentPlayer = this.player1;
+      }
+      const oppositePlayer =
+        this.currentPlayer === this.player1 ? this.player2 : this.player1;
+      if (
+        typeof clickedField === 'number' &&
+        !this.checkWin(this.board, oppositePlayer)
+      ) {
+        this.board.setField(this.currentPlayer, num);
+        if (this.checkWin(this.board, this.currentPlayer)) {
+          this.endGame(this.currentPlayer);
+        } else if (this.checkDraw(this.board)) {
+          this.endGame('draw');
+        }
+
+        // afterwards switch the players
+        this.currentPlayer = oppositePlayer;
+        `${this.currentPlayer.getMark()}'s turn`;
+      }
+    }
+    return;
+  };
+
+  computerMove() {
+    const bestStepIndex = minimax.call(this, this.board, this.computer).index;
+    console.log(bestStepIndex);
+    this.board.setField(this.computer, bestStepIndex);
+
+    // check if computer is already won or it's draw, otherwise, it's player's turn
+    if (this.checkWin(this.board, this.computer)) {
+      return this.endGame(this.computer);
+    } else if (this.checkDraw(this.board)) {
+      return this.endGame('draw');
+    }
+    return;
+  }
+
+  changeMark(mark, mode) {
     if (mode === 'computer') {
       if (mark === 'x') {
-        _player1 = Player('x');
-        _computer = Player('o');
+        this.player1 = new Player('x');
+        this.computer = new Player('o');
       } else if (mark === 'o') {
-        _player1 = Player('o');
-        _computer = Player('x');
+        this.player1 = new Player('o');
+        this.computer = new Player('x');
       }
     } else if (mode === 'multiple') {
       if (mark === 'x') {
-        _player1 = Player('x');
-        _player2 = Player('o');
+        this.player1 = new Player('x');
+        this.player2 = new Player('o');
       } else if (mark === 'o') {
-        _player1 = Player('o');
-        _player2 = Player('x');
+        this.player1 = new Player('o');
+        this.player2 = new Player('x');
       }
     }
-  };
+  }
 
-  const playerMove = num => {
-    const clickedField = GameBoard.getField(num);
-    // only fill the field if empty
-    if (_mode === 'computer') {
-      if (
-        typeof clickedField === 'number' &&
-        !_checkWin(GameBoard, _computer)
-      ) {
-        GameBoard.setField(_player1, num);
-        // check if player is already won or it's draw, otherwise, it's computer's turn
-        if (_checkWin(GameBoard, _player1)) {
-          endGame(_player1);
-        } else if (_checkDraw(GameBoard)) {
-          endGame('draw');
-        } else {
-          setTimeout(computerMove, 500);
-        }
-      }
-    } else if (_mode === 'multiple') {
-      const oppositePlayer = currentPlayer === _player1 ? _player2 : _player1;
-      if (
-        typeof clickedField === 'number' &&
-        !_checkWin(GameBoard, oppositePlayer)
-      ) {
-        GameBoard.setField(currentPlayer, num);
-
-        if (_checkWin(GameBoard, currentPlayer)) {
-          endGame(currentPlayer);
-        } else if (_checkDraw(GameBoard)) {
-          endGame('draw');
-        }
-        // afterwards switch the players
-        currentPlayer = oppositePlayer;
-        `${currentPlayer.getMark()}'s turn`;
-      }
-    }
-
-    return;
-  };
-
-  const computerMove = () => {
-    const bestStepIndex = minimax(GameBoard, _computer).index;
-    GameBoard.setField(_computer, bestStepIndex);
-    // check if computer is already won or it's draw, otherwise, it's player's turn
-    if (_checkWin(GameBoard, _computer)) {
-      return endGame(_computer);
-    } else if (_checkDraw(GameBoard)) {
-      return endGame('draw');
-    }
-    return;
-  };
-
-  const startGame = () => {
-    // before game, clear the board and UI
-    GameBoard.resetBoard();
-    UIController.clearUI();
-    if (_mode === 'computer') {
-      if (_player1.getMark() === 'o') {
-        computerMove();
-      }
-    } else if (_mode === 'multiple') {
-      // setting the currentPlayer
-      _player1.getMark() === 'o'
-        ? (currentPlayer = _player2)
-        : (currentPlayer = _player1);
-    }
-  };
-
-  // checking the state of game : draw or win possibilities
-
-  // const _allFilledFields = GameBoard.availableFields().length === 0;
-
-  const _checkDraw = board => {
-    // if we have win or all fields are not filled,return false,otherwise true
+  // states of the game (DRAW OR WIN)
+  checkDraw(board) {
+    // if we have won or all fields are not filled, return false, otherwise true
     // checking for draw based on mode (either player2 or computer)
-    if (_player2) {
-      if (_checkWin(board, _player1) || _checkWin(board, _player2)) {
+    // multiple
+    if (this.player2) {
+      if (
+        this.checkWin(board, this.player1) ||
+        this.checkWin(board, this.player2)
+      ) {
         return false;
       }
-    } else if (_computer) {
-      if (_checkWin(board, _player1) || _checkWin(board, _computer)) {
+    } else if (this.computer) {
+      if (
+        this.checkWin(board, this.player1) ||
+        this.checkWin(board, this.computer)
+      ) {
         return false;
       }
     }
@@ -192,121 +233,109 @@ const GameController = (() => {
       }
     }
     return true;
-  };
+  }
 
   // if [1,2,3] have the same mark [x,x,x] or [o,o,o] from winning-combination array
-  const _checkWin = (board, player) => {
-    return _winningCombinations.some(combo => {
+  checkWin(board, player) {
+    return Game.WINNING_COMBINATION.some(combo => {
       return combo.every(index => board.getField(index) === player.getMark());
     });
-  };
+  }
 
-  const endGame = player => {
+  endGame(player) {
     if (player === 'draw') {
+      // show the message on UI
       console.log('draw');
-      UIController.gameOverMessage('draw');
+      this.gameOverMessage('draw');
       return 'draw';
     } else {
       let winner = player.getMark();
-      UIController.gameOverMessage(winner);
+      // show the message on UI
+      console.log(`${winner} has won the game`);
+      this.gameOverMessage(winner);
       return `${winner} has won the game`;
     }
-  };
+  }
 
-  // current board and current player
-  const minimax = (board, player) => {
-    const emptySpots = board.availableFields();
-    // base case : if any of these are true, the current recursion should be stopped
-    if (_checkDraw(board)) {
-      return { score: 0 };
-    } else if (_checkWin(board, _player1)) {
-      return { score: -10 };
-    } else if (_checkWin(board, _computer)) {
-      return { score: 10 };
+  gameOverMessage(player) {
+    const winner = document.querySelector('.winner');
+    messageBox.style.transform = 'scale(1)';
+    messageBox.classList.add('show');
+    gameDom.style.filter = 'blur(5px)';
+    winner.textContent =
+      player !== 'draw' ? `${player.toUpperCase()} has won` : 'Draw';
+  }
+}
+
+// current board and current player
+function minimax(board, player) {
+  const emptySpots = board.getAvailableFields();
+  // base case : if any of these are true, the current recursion should be stopped
+  if (this.checkDraw(board)) {
+    return { score: 0 };
+  } else if (this.checkWin(board, this.player1)) {
+    return { score: -10 };
+  } else if (this.checkWin(board, this.computer)) {
+    return { score: 10 };
+  }
+  // stores the moves for each field
+  const moves = [];
+
+  for (let i = 0; i < emptySpots.length; i++) {
+    // individual move,should have index and score of a given empty field
+    let move = {};
+    // set the index of the board.availableFields() field to the move
+    move.index = emptySpots[i];
+    // fill board.availableFields() field with current player
+    board.setTempField(emptySpots[i], player);
+    // if player is computer, call minimax with _player1
+    if (player === this.computer) {
+      let result = minimax.call(this, this.board, this.player1);
+      move.score = result.score;
     }
-    // stores the moves for each field
-    const moves = [];
-
-    for (let i = 0; i < emptySpots.length; i++) {
-      // individual move,should have index and score of a given empty field
-      let move = {};
-      // set the index of the board.availableFields() field to the move
-      move.index = emptySpots[i];
-      // fill board.availableFields() field with current player
-      board.setTempField(emptySpots[i], player);
-      // if player is computer, call minimax with _player1
-      if (player === _computer) {
-        let result = minimax(board, _player1);
-        move.score = result.score;
-      }
-      // otherwise, call it with _computer
-      else if (player === _player1) {
-        let result = minimax(board, _computer);
-        move.score = result.score;
-      }
-      // reset the board to be empty again(with number)
-      board.setTempField(emptySpots[i], undefined);
-      moves.push(move);
+    // otherwise, call it with _computer
+    else if (player === this.player1) {
+      let result = minimax.call(this, this.board, this.computer);
+      move.score = result.score;
     }
+    // reset the board to be empty again(with number)
+    this.board.setTempField(emptySpots[i], undefined);
+    moves.push(move);
+  }
 
-    // find the best step based on player
-    let bestStep;
+  // find the best step based on player
+  let bestStep;
 
-    if (player === _computer) {
-      let bestScore = -10000;
-      for (let i = 0; i < moves.length; i++) {
-        if (moves[i].score > bestScore) {
-          bestScore = moves[i].score;
-          bestStep = i;
-        }
-      }
-    } else if (player === _player1) {
-      let bestScore = 10000;
-      for (let i = 0; i < moves.length; i++) {
-        if (moves[i].score < bestScore) {
-          bestScore = moves[i].score;
-          bestStep = i;
-        }
+  if (player === this.computer) {
+    let bestScore = -10000;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestStep = i;
       }
     }
-    return moves[bestStep];
-  };
+  } else if (player === this.player1) {
+    let bestScore = 10000;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestStep = i;
+      }
+    }
+  }
+  return moves[bestStep];
+}
 
-  return {
-    getPlayer1,
-    getComputer,
-    setMode,
-    getMode,
-    changeMark,
-    playerMove,
-    startGame
-  };
-})();
+const game = new Game();
 
-// controls UI
-const UIController = (() => {
+document.addEventListener('DOMContentLoaded', e => {
   const modeOptions = document.querySelectorAll('.option');
-  const playAgainBtn = document.querySelector('.play-again');
   const players = document.querySelectorAll('.player');
-  const fields = [...document.querySelectorAll('.field')];
-  const game = document.querySelector('.game');
-  const messageBox = document.querySelector('.message-box');
 
-  const _initialLoad = () => {
-    fields.forEach((field, i) =>
-      field.addEventListener('click', GameController.playerMove.bind(this, i))
-    );
-    GameBoard.resetBoard();
-    messageBox.classList.remove('show');
-    messageBox.classList.add('hide');
-    game.style.filter = 'blur(0)';
-    document.querySelector('.options').style.display = 'flex';
-  };
+  initialLoad();
 
-  _initialLoad();
-
-  const _chooseMode = mode => {
-    GameController.setMode(mode);
+  const chooseMode = mode => {
+    game.setMode(mode);
     // only on larger screens display mode and player settings at the same time
     if (window.innerWidth <= 450 && window.innerWidth >= 320) {
       setTimeout(() => {
@@ -322,51 +351,21 @@ const UIController = (() => {
     }, 500);
   };
 
-  const _chooseMark = mark => {
-    GameController.changeMark(mark, GameController.getMode());
+  modeOptions.forEach(mode =>
+    mode.addEventListener('click', chooseMode.bind(this, mode.id))
+  );
+
+  const chooseMark = mark => {
+    game.changeMark(mark, game.getMode());
 
     setTimeout(() => {
       document.querySelector('.game').style.display = 'flex';
       document.querySelector('.settings').style.display = 'none';
-      GameController.startGame();
+      game.startGame();
     }, 200);
   };
 
-  const clearUI = () => {
-    fields.forEach(field => (field.textContent = ''));
-  };
-
-  modeOptions.forEach(mode =>
-    mode.addEventListener('click', _chooseMode.bind(this, mode.id))
-  );
-
-  players.forEach(player =>
-    player.addEventListener('click', _chooseMark.bind(this, player.id))
-  );
-
-  // reset everything (board, ui, hide the board)
-  playAgainBtn.addEventListener('click', e => {
-    e.target.parentNode.classList.remove('show');
-    setTimeout(() => {
-      clearUI();
-      _initialLoad();
-      // show settings and hide board and players option
-      document.querySelector('.game').style.display = 'none';
-      document.querySelector('.settings').style.display = 'flex';
-      document.querySelector('.players').style.display = 'none';
-    }, 500);
+  players.forEach(player => {
+    player.addEventListener('click', chooseMark.bind(game, player.id));
   });
-
-  const gameOverMessage = player => {
-    const winner = document.querySelector('.winner');
-    messageBox.style.transform = 'scale(1)';
-    messageBox.classList.add('show');
-    game.style.filter = 'blur(5px)';
-    winner.textContent =
-      player !== 'draw' ? `${player.toUpperCase()} has won` : 'Draw';
-  };
-  return {
-    clearUI,
-    gameOverMessage
-  };
-})();
+});
